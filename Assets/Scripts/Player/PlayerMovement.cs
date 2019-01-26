a4fc47703dabae52e9a8d8ef081a2635bd8255f7
pickupst <gd.okanuslu@gmail.com>
2019-01-26 10:02:58 +0300

    Add player move rotation and camera position

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,12 +5,20 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public float speed = 6f;
+    Animator animator;
+
+
     Vector3 movement;
     Rigidbody playerRigidbody;
+    int floorMask;
+    float camRayLength = 100f;
+    float rotationLerpCount = 4f;
 
     private void Awake()
     {
         playerRigidbody = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+        floorMask = LayerMask.GetMask("Floor");
     }
 
 
@@ -21,6 +29,34 @@ public class PlayerMovement : MonoBehaviour
         float v = Input.GetAxis("Vertical");
         Move(h, v);
 
+        Animating(h, v);
+
+        Turning();
+
+    }
+
+    private void Turning()
+    {
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit floorHit;
+
+        if (Physics.Raycast(camRay, out floorHit, camRayLength, floorMask))
+        {
+
+            Vector3 playerToMOuse = floorHit.point - transform.position;
+            playerToMOuse.y = 0;
+            Quaternion newRotation = Quaternion.LookRotation(playerToMOuse);
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, Time.deltaTime * rotationLerpCount);
+            //playerRigidbody.MoveRotation(newRotation);
+
+        }
+    }
+
+    private void Animating(float h, float v)
+    {
+        bool walking = h != 0f || v != 0;
+        animator.SetBool("isWalking", walking);
     }
 
     private void Move(float h, float v)
