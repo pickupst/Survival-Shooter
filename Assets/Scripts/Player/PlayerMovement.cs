1cad5e889bd7d19dee2b4fc402a09a3750356a61
pickupst <gd.okanuslu@gmail.com>
2019-01-26 09:39:54 +0300

    Add camera movement with LERP

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,6 +1,32 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    
+
+    public float speed = 6f;
+    Vector3 movement;
+    Rigidbody playerRigidbody;
+
+    private void Awake()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+
+    private void FixedUpdate()
+    {
+
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Move(h, v);
+
+    }
+
+    private void Move(float h, float v)
+    {
+        movement.Set(h, 0, v);
+        movement = movement * Time.deltaTime * speed;
+        playerRigidbody.MovePosition(transform.position + movement);
+    }
 }
